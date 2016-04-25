4d13aa0a37bff8ee954843fa1e69fdb6c2304b02
Byrddawg <shane_byrd1991@hotmail.com>
2016-04-25 07:46:22 -0600

    LoadLevel.cs

---
+++ LoadLevel.cs
@@ -0,0 +1,9 @@
+//Created by Shane Byrd @ Byrddawg Technologies\\
+//This code is an example of problem solving\\
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadLevel : MonBehaviour {
+public void Level(string name) { 
+  Application.LoadLevel("Example")
